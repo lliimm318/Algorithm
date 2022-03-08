d20f5c598cf2f6321a80fc3bd797c0318a282c0a
임서영 <sylim0318@naver.com>
2022-03-08 14:30:21 +0900

    [Level1] 로또 최고 순위 최저 순위

---
+++ level1/normal/lotto/Main.java
@@ -0,0 +1,30 @@
+package com.company.Algorithm.level1.normal.lotto;
+// 로또의 최고 순위와 최저 순위
+public class Main {
+    public static void main(String[] args) {
+
+        int[] lottos = {44, 1, 0, 0, 31, 25};
+        int[] win_nums = {31, 10, 45, 1, 6, 19};
+
+        int[] answer = new int[2];
+        int[] rank = {6, 6, 5, 4, 3, 2, 1};
+
+        int max = 0;
+        int min = 0;
+
+        for(int i = 0; i < lottos.length; i++) {
+            for (int j = 0; j < lottos.length; j++) {
+                if(lottos[i] == win_nums[j]) {
+                    min++;
+                }
+            }
+            if (lottos[i] == 0) {
+                max++;
+            }
+        }
+        max += min;
+
+        answer[0] = rank[max];
+        answer[1] = rank[min];
+    }
+}
