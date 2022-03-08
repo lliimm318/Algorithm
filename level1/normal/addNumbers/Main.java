5f495470245d32a691c1e39473d7bcecb5e77af5
임서영 <sylim0318@naver.com>
2022-03-08 14:29:57 +0900

    [Level1] 없는 숫자 더하기

---
+++ level1/normal/addNumbers/Main.java
@@ -0,0 +1,16 @@
+package com.company.Algorithm.level1.normal.addNumbers;
+//없는 숫자 더하기
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 6, 7, 8, 0};
+        int answer = 0;
+
+        for(int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+        }
+
+        System.out.println(45 - answer);
+
+    }
+}
