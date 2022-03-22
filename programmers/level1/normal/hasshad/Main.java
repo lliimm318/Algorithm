7979b75c2cdb2b4455b7c2735ad98f4d8658ef7d
임서영 <sylim0318@naver.com>
2022-03-23 08:48:57 +0900

    [프로그래머스] level 1 하샤드 수

---
+++ programmers/level1/normal/hasshad/Main.java
@@ -0,0 +1,24 @@
+package com.company.Algorithm.programmers.level1.normal.hasshad;
+
+// 하샤드 수
+public class Main {
+    public static void main(String[] args) {
+        int x = 13;
+
+        int a = x;
+
+        boolean answer = true;
+        int sum = 0;
+
+        while (a != 0) {
+            sum += a % 10;
+            a /= 10;
+        }
+
+        if(x % sum == 0) {
+            System.out.println(answer);
+        } else {
+            System.out.println("false");
+        }
+    }
+}
