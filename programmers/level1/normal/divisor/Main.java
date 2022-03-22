98dab474a113230bb81cf56237ca6f65f2870820
임서영 <sylim0318@naver.com>
2022-03-23 08:46:38 +0900

    [프로그래머스] level1 약수의 개수와 덧셈

--- programmers/level1/normal/divisor/Main.java
+++ programmers/level1/normal/divisor/Main.java
@@ -9,10 +9,16 @@ public class Main {
         int answer = 0;
 
         for(int i = left; i <= right; i++) {
-            for(int j = 1; j <= i/2; j ++) {
-                if(j*j == i) {
-
+            int cnt = 0;
+            for(int j = 1; j <= i; j ++) {
+                if(i%j == 0) {
+                    cnt++;
                 }
+            }
+            if(cnt%2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
             }
         }
 
