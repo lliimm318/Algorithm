9f4bc28b30d2461ab6e94e91f458a247be98ae33
임서영 <sylim0318@naver.com>
2022-03-25 08:28:20 +0900

    [프로그래머스] level1 이상한 문자 만들기

---
+++ programmers/level1/normal/weird/Main.java
@@ -0,0 +1,24 @@
+package com.company.Algorithm.programmers.level1.normal.weird;
+
+import java.util.Locale;
+
+//이상한 문자 구하기
+public class Main {
+    public static void main(String[] args) {
+        String s = "try hello world";
+
+        String answer = "";
+
+        String[] arr = s.split("");
+
+        for(int i = 0; i < s.length(); i++) {
+            if(i%2 == 0) {
+               arr[i].toUpperCase();
+            }
+            answer.concat(arr[i]);
+        }
+
+        System.out.println(answer);
+
+    }
+}
