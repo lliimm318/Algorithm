e564b4ad2c4f55f1e8d61de0df5f4e111086168f
임서영 <sylim0318@naver.com>
2022-03-28 19:47:23 +0900

    [프로그래머스] level1 문자열 내림차순으로 정렬

--- programmers/level1/normal/descending/Main.java
+++ programmers/level1/normal/descending/Main.java
@@ -1,19 +1,22 @@
 package com.company.Algorithm.programmers.level1.normal.descending;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 //문자열 내림차순으로 정렬하기
 public class Main {
     public static void main(String[] args) {
         String s = "Zbcdefg";
         String answer = "";
 
-        char[] arr = new char[s.length()];
-        int a = 0;
-        for(int i = s.length(); i > 0; i--){
-            arr[a] = s.charAt(i);
-            a++;
-        }
-        answer = String.valueOf(arr);
-        System.out.println(answer);
+        String[] arr = s.split("");
 
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+
+        System.out.println(answer);
     }
 }
