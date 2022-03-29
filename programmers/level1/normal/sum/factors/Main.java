6e2e20c048cb158fa57735650414a41b099a42ab
임서영 <sylim0318@naver.com>
2022-03-30 07:56:02 +0900

    [프로그래머스] level 1 약수의 합

---
+++ programmers/level1/normal/sum/factors/Main.java
@@ -0,0 +1,16 @@
+package com.company.Algorithm.programmers.level1.normal.sum.factors;
+
+//약수의 합
+public class Main {
+    public static void main(String[] args) {
+        int n = 12;
+        int answer = 0;
+
+        for(int i = 1; i<n+1; i++) {
+            if(n%i == 0) {
+                answer += i;
+            }
+        }
+        System.out.println(answer);
+    }
+}
