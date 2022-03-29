ffa33f6e21a3a86d68c722ca83cbeb51e3ad7ea0
임서영 <sylim0318@naver.com>
2022-03-29 09:47:18 +0900

    [프로그래머스] level 1 같은 문자열 다루기 기본

---
+++ programmers/level1/normal/handling/Main.java
@@ -0,0 +1,23 @@
+package com.company.Algorithm.programmers.level1.normal.handling;
+
+//문자열 다루기 기본
+public class Main {
+    public static void main(String[] args) {
+        String s = "a1234";
+        boolean answer = true;
+
+        if(s.length() == 4 || s.length() == 6) {
+
+            for (int i = 0; i < s.length(); i++) {
+                if (!Character.isDigit(s.charAt(i))) {
+                    answer = false;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
+
+
+// Character.isDigit() -> 입력 값이 숫자인지 여부 판단
+// Character.isLetter() -> 입력 값이 문자인지 여부 판단
