7f1a9e33cb2afe091ed1cf0c60d44213c7ba3a61
임서영 <sylim0318@naver.com>
2022-03-30 07:58:25 +0900

    [프로그래머스] level 1 문자열 정수로 바꾸기

---
+++ programmers/level1/normal/change/num/Main.java
@@ -0,0 +1,11 @@
+package com.company.Algorithm.programmers.level1.normal.change.num;
+
+//문자열 정수로 바꾸기
+public class Main {
+    public static void main(String[] args) {
+        String s = "-11234";
+
+        int answer = Integer.parseInt(s);
+        System.out.println(answer);
+    }
+}
