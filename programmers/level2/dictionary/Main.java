869a19ae8a4e1ca4d603ef94cdcb54ffd3ec46bc
임서영 <sylim0318@naver.com>
2022-04-12 10:08:49 +0900

    [프로그래머스] level2 모음사전

---
+++ programmers/level2/dictionary/Main.java
@@ -0,0 +1,19 @@
+package com.company.Algorithm.programmers.level2.dictionary;
+
+//모음사전
+public class Main {
+    public static void main(String[] args) {
+        String word = "E";
+
+        int answer = 0;
+        int max= 3905;
+
+        String dictionary = "AEIOU";
+
+        for(String s : word.split("")) {
+            answer += dictionary.indexOf(s) * (max / 5) +1;
+        }
+        System.out.println(answer);
+
+    }
+}
