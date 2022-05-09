7d37677ee1b4ef25ac92bd9dc6a6492bc137f714
임서영 <sylim0318@naver.com>
2022-05-09 10:34:42 +0900

    옆을 보고 있는 고양이

---
+++ cording/test/cat/side/Main.java
@@ -0,0 +1,11 @@
+package com.company.Algorithm.cording.test.cat.side;
+
+//백준 10171번 고양이
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
