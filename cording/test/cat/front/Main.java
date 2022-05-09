034dfb6cef0a3661dbaa88c373d1c834b601fa62
임서영 <sylim0318@naver.com>
2022-05-09 10:34:27 +0900

    앞을 보고 있는 고양이

---
+++ cording/test/cat/front/Main.java
@@ -0,0 +1,10 @@
+package com.company.Algorithm.cording.test.cat.front;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  /\\  /\\");
+        System.out.println("=( '- ' )=(");
+        System.out.println("  |     |  )");
+        System.out.println(" ( _| |_ )/");
+    }
+}
