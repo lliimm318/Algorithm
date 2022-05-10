38b4bc20d7272b7be06516318907da4ac886ef2e
임서영 <sylim0318@naver.com>
2022-05-10 09:22:57 +0900

    놀란 고양이

---
+++ cording/test/cat/walk/Main.java
@@ -0,0 +1,12 @@
+package com.company.Algorithm.cording.test.cat.walk;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  |\\");
+        System.out.println("  ||   /\\_/|");
+        System.out.println("  \\\\ =| 0 0 )=");
+        System.out.println("   ||_|     \\");
+        System.out.println("  /      __ \\");
+        System.out.println("  |_)  \\_)  \\_)");
+    }
+}
