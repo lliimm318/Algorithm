6f9924afa073692bd4475bea1acfdb9bd653ebd0
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-15 08:42:51 +0900

    Create Main.java

---
+++ programmers/level0/chicken/Main.java
@@ -0,0 +1,18 @@
+package com.company.Algorithm.programmers.level0.chicken;
+
+// 120884번 치킨쿠폰
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static int solution(int chicken) {
+        int answer = 0;
+        int c = 0;
+
+        while (chicken != 0) {
+            chicken
+        }
+
+        return answer;
+    }
+}
