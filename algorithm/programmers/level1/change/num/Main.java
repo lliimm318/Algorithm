b4f9428eb4a90a72c0e6711b0eedd9d6a18db8cf
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 08:56:54 +0900

    [프로그래머스] 12925번 문자열 정수로 바꾸기
    https://school.programmers.co.kr/learn/courses/30/lessons/12925

---
+++ algorithm/programmers/level1/change/num/Main.java
@@ -0,0 +1,11 @@
+package com.company.Algorithm.algorithm.programmers.level1.change.num;
+
+// 1292번 문자열 정수로 바꾸기
+public class Main {
+    public static void main(String[] args) {
+        String s = "-11234";
+
+        int answer = Integer.parseInt(s);
+        System.out.println(answer);
+    }
+}
