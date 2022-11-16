1e7c99c67317dde55fac4b155e60fd9ea70db6a8
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 10:02:07 +0900

    [프로그래머스] 76501번 음향 더하기
    https://school.programmers.co.kr/learn/courses/30/lessons/76501

---
+++ algorithm/programmers/level1/sound/Main.java
@@ -0,0 +1,26 @@
+package com.company.Algorithm.algorithm.programmers.level1.sound;
+
+// 76501번 음향 더하기
+public class Main {
+    public static void main(String[] args) {
+
+        int[] absolutes = {4, 7, 12};
+        boolean[] signs = {true, false, true};
+        int[] arr = new int[signs.length];
+        int answer = 0;
+
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i] == false) {
+                arr[i] = -1;
+            } else {
+                arr[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += absolutes[i] * arr[i];
+        }
+
+        System.out.println(answer);
+    }
+}
