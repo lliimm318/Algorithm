440f33d892213cc7d6f00b66a89df384a127f963
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 10:10:27 +0900

    [프로그래머스] 12954번 x만큼 간격이 있는 n 개의 숫자
    https://school.programmers.co.kr/learn/courses/30/lessons/12954

---
+++ algorithm/programmers/level1/xinterval/Main.java
@@ -0,0 +1,16 @@
+package com.company.Algorithm.algorithm.programmers.level1.xinterval;
+
+//x만큼 간격이 있는 n개의 숫자
+public class Main {
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+
+        long[] answer = new long[n];
+
+        for(int i = 0; i < n; i++) {
+            answer[i] += x * i + x;
+            System.out.println(answer[i]);
+        }
+    }
+}
