b5de07c68154683695b88961330020044d7bc2f3
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 10:06:25 +0900

    [프로그래머스 12930번 이상한 문자 구하기
    https://school.programmers.co.kr/learn/courses/30/lessons/12930

---
+++ algorithm/programmers/level1/weird/Main.java
@@ -0,0 +1,26 @@
+package com.company.Algorithm.algorithm.programmers.level1.weird;
+
+// 12930번 이상한 문자 구하기
+public class Main {
+    public static void main(String[] args) {
+        String s = "try hello world";
+
+        String answer = "";
+
+        String[] arr = s.toLowerCase().split(" ");
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length(); j++) {
+                if (j % 2 == 0) {
+                    answer += (arr[i].charAt(j) + "").toUpperCase();
+                } else {
+                    answer += (arr[i].charAt(j) + "").toLowerCase();
+                }
+            }
+            answer += " ";
+        }
+        answer = answer.substring(0, answer.length()-1);
+        System.out.println(answer);
+
+    }
+}
