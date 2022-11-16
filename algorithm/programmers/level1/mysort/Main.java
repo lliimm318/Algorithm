3950dd43a50e1031889c557e34a93fe5e92b8fcb
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 09:54:22 +0900

    [프로그래머스] 12915번 문자열 내 마음대로 정렬하기
    https://school.programmers.co.kr/learn/courses/30/lessons/12915

---
+++ algorithm/programmers/level1/mysort/Main.java
@@ -0,0 +1,29 @@
+package com.company.Algorithm.algorithm.programmers.level1.mysort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// 12915번 문자열 내 마음대로 정렬하기
+public class Main {
+    public static void main(String[] args) {
+        String[] strings = {"sun", "bed", "car"};
+        int n = 1;
+
+        String[] answer = {};
+
+        ArrayList<String> arr = new ArrayList<>();
+
+        for (int i = 0; i < strings.length; i++) {
+            arr.add("" + strings[i].charAt(n) + strings[i]);
+        }
+
+        Collections.sort(arr);
+        answer = new String[arr.size()];
+
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i).substring(1, arr.get(i).length());
+            System.out.println(answer[i]);
+        }
+
+    }
+}
