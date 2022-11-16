2ad2fc065ef61a54876f7d7bd49f57233d158da8
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 09:53:39 +0900

    [프로그래머스] 12935번 제일 작은 수 제거하기
    https://school.programmers.co.kr/learn/courses/30/lessons/12935

---
+++ algorithm/programmers/level1/most/small/Main.java
@@ -0,0 +1,32 @@
+package com.company.Algorithm.algorithm.programmers.level1.most.small;
+
+// 12935번 제일 작인 수 제거하기
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 1};
+        int[] answer = new int[arr.length-1];
+
+        int min = arr[0];
+
+        if (arr.length <= 1) {
+            int[] answer2 = { -1 };
+            System.out.println(answer2);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        int index = 0;
+        for (int j = 0; j < arr.length; j++) {
+            if (min == arr[j]) {
+                continue;
+            } else {
+                answer[index++] = arr[j];
+            }
+        }
+        System.out.println(answer);
+    }
+}
