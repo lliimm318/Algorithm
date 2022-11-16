9dd7b9b4de7d57cc1ddb0a9aea519b92b236f3bd
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 10:25:17 +0900

    [프로그래머스]

---
+++ algorithm/programmers/level2/target/number/Main.java
@@ -0,0 +1,20 @@
+package com.company.Algorithm.algorithm.programmers.level2.target.number;
+
+//타겟넘버
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1,1,1,1,1};
+        int target = 3;
+
+        int answer = dfs(numbers, 0, 0, target);
+
+    }
+    public static int dfs(int[] numbers, int depth, int sum, int target) {
+        if(depth== numbers.length) {
+            if(target == sum) return 1;
+            else return 0;
+        } else
+            return dfs(numbers,depth+1, sum+numbers[depth], target)
+            + dfs(numbers, depth+1, sum-numbers[depth],target);
+    }
+}
