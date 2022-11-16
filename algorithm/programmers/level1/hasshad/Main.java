c3c44c0e8153a03803090b9926e05bde9dc06580
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 09:43:22 +0900

    [프로그래머스] 12947번 하샤드수
    https://school.programmers.co.kr/learn/courses/30/lessons/12947

---
+++ algorithm/programmers/level1/hasshad/Main.java
@@ -0,0 +1,24 @@
+package com.company.Algorithm.algorithm.programmers.level1.hasshad;
+
+// 12947번 하샤드 수
+public class Main {
+    public static void main(String[] args) {
+        int x = 13;
+
+        int a = x;
+
+        boolean answer = true;
+        int sum = 0;
+
+        while (a != 0) {
+            sum += a % 10;
+            a /= 10;
+        }
+
+        if(x % sum == 0) {
+            System.out.println(answer);
+        } else {
+            System.out.println("false");
+        }
+    }
+}
