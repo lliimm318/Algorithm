42c4c555f90f076ab9e09c12bb0ef8ef2ec8b5f1
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 09:06:23 +0900

    [프로그래머스] 12910번 나누어 떨어지는 숫자배열
    https://school.programmers.co.kr/learn/courses/30/lessons/12910

---
+++ algorithm/programmers/level1/divid/arr/Main.java
@@ -0,0 +1,32 @@
+package com.company.Algorithm.algorithm.programmers.level1.divid.arr;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// 12910번 나누어 떨어지는 숫자 배열
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5,9,7,10};
+        int divisor = 5;
+
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                list.add(arr[i]);
+            }
+        }
+
+        if(list.isEmpty()) {
+            list.add(-1);
+        }
+
+        answer = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] += list.get(i);
+        }
+        Arrays.sort(answer);
+    }
+}
