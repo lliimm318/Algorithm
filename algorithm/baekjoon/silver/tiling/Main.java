ef8e7e80939e199c29ba04397e541dc0d646363b
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 13:30:24 +0900

    [백준] 11726번 2n 타일링
    https://www.acmicpc.net/problem/11726

--- algorithm/baekjoon/silver/tiling/Main.java
+++ algorithm/baekjoon/silver/tiling/Main.java
@@ -1,8 +1,24 @@
 package com.company.Algorithm.algorithm.baekjoon.silver.tiling;
 
-// 11
+import java.util.Scanner;
+
+// 11726번 2n타일링
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] dp = new int[n+1];
 
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+        System.out.println(dp[n]);
     }
 }
