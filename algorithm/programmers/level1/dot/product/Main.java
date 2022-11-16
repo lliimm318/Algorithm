d7c2d3425a5319336c27c45ba590a4fabc974756
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 09:41:47 +0900

    [프로그래머스] 70128번 내적
    https://school.programmers.co.kr/learn/courses/30/lessons/70128

---
+++ algorithm/programmers/level1/dot/product/Main.java
@@ -0,0 +1,18 @@
+package com.company.Algorithm.algorithm.programmers.level1.dot.product;
+
+// 70128번 내적
+public class Main {
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3, 4};
+        int[] b = {-3, -1, 0, 2};
+
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        System.out.println(answer);
+    }
+}
