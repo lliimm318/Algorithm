ea378281c3dec7d5ceb3717fcc0bb062f6826fca
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-16 09:58:43 +0900

    [프로그래머스] 12916번 문자열 내 p와  y의 개수
    https://school.programmers.co.kr/learn/courses/30/lessons/12916

---
+++ algorithm/programmers/level1/py/Main.java
@@ -0,0 +1,26 @@
+package com.company.Algorithm.algorithm.programmers.level1.py;
+
+// 12916번 문자열 내 p와 y의 개수
+public class Main {
+    public static void main(String[] args) {
+        String s = "pPoooy";
+        boolean answer = true;
+
+        int cnt = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.substring(i, i+1).equals("p") || s.substring(i,i+1).equals("P")) {
+                cnt++;
+            }
+            else if(s.substring(i, i+1).equals("y") || s.substring(i,i+1).equals("Y")) {
+                cnt--;
+            }
+
+        }
+        if(cnt !=0 ) {
+            answer = false;
+        }
+
+        System.out.println(answer);
+    }
+}
