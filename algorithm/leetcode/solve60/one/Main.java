f82bf98de580f1328c573517be1635840889f05e
임서영 <seoyoung@fassto.com>
2024-04-15 08:51:10 +0900

    리트코드 two sum

---
+++ algorithm/leetcode/solve60/one/Main.java
@@ -0,0 +1,33 @@
+package com.company.Algorithm.algorithm.leetcode.solve60.one;
+
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/?envType=list&envId=xo2bgr0r
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        twoSum(nums, target);
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        boolean isEnd = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++){
+                if (target == nums[i] + nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    isEnd = true;
+                    break;
+                }
+            }
+            if (isEnd) {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
